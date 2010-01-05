ae33ec1104e849452865ffd01a30fecfd490e336
Zoozy.cn <Zoozy.cn@c3112bca-f690-11de-91a0-e5abc95b34ea>
2010-01-05 03:46:41 +0000

    Demo version 0.3 Previous for Mr.Zhu.

---
+++ trunk/Chaining.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Demo
+{
+    /// <summary>
+    /// Chaining.xaml 的交互逻辑
+    /// </summary>
+    public partial class Chaining : Page
+    {
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            // go back
+            this.NavigationService.Navigate(new Uri("GameMain.xaml", UriKind.Relative));
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ trunk/Register.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace Demo
+{
+    /// <summary>
+    /// Register.xaml 的交互逻辑
+    /// </summary>
+    public partial class Register : Page
+    {
+        private void btnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            // save setting and start game
+            App.ConfigClass.SaveConfig("sUserName", this.txtUsername.Text);
+
+            this.NavigationService.Navigate(new Uri("GameMain.xaml", UriKind.Relative));
+        }
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("Menu.xaml", UriKind.Relative));
+        }
+    }
+}
